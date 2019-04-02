9e28189db18b8d2b1c0b72ccd2a6617798834d0d
Tobias Lundgren <tobias@lundgren.design>
2019-04-03 00:56:05 +0200

    Init commit

---
+++ README.md
@@ -0,0 +1,10 @@
+# Planning poker server
+
+## Features
+
+- GraphQL: Apollo server express
+- Babel preset: stage-2
+- Monoose
+- JWT + bcrypt
+- axios
+- nodemon
---
+++ src/index.js
@@ -0,0 +1,30 @@
+import express from 'express'
+import { ApolloServer, gql } from 'apollo-server-express'
+
+// The GraphQL schema
+const typeDefs = gql`
+  type Query {
+    "A simple type for getting started!"
+    hello: String
+  }
+`
+
+// A map of functions which return data for the schema.
+const resolvers = {
+  Query: {
+    hello: () => 'world',
+  },
+}
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+})
+
+// Apply apollo server middleware
+const app = express()
+server.applyMiddleware({ app })
+
+app.listen({ port: 4000 }, () =>
+  console.log(`🚀 Server ready at http://localhost:4000${server.graphqlPath}`)
+)
