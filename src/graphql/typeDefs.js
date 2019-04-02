75307a4f553a3c800e3ae5fdf1db4a060c41959f
Tobias Lundgren <tobias@lundgren.design>
2019-04-03 01:33:15 +0200

    Add graphql schema

--- src/graphql/typeDefs.js
+++ src/graphql/typeDefs.js
@@ -2,8 +2,43 @@ import { gql } from 'apollo-server-express'
 
 const typeDefs = gql`
   type Query {
-    "A simple type for getting started!"
     hello: String
+    stories: [Story!]!
+    story(id: ID!): Story
+    me: User
+    users: [User!]!
+  }
+
+  type Mutation {
+    signup(email: String!, password: String!, name: String!): AuthPayload!
+    login(email: String!, password: String!): AuthPayload!
+    createStory(title: String, content: String): Story!
+    deleteStory(id: ID!): Story!
+  }
+
+  type Subscription {
+    storySubscription: Story
+  }
+
+  type AuthPayload {
+    token: String!
+    user: User!
+  }
+
+  type User {
+    id: ID!
+    email: String!
+    name: String!
+    stories: [Story!]!
+  }
+
+  type Story {
+    id: ID!
+    active: Boolean
+    title: String!
+    content: String
+    estimate: Float
+    author: User!
   }
 `
 export default typeDefs
