7cbe80cd84c1b3ee4b4c585e7b87fd99bb5ff4df
Tobias Lundgren <tobias@lundgren.design>
2019-04-03 14:02:00 +0200

    Add user query

--- src/graphql/resolvers/Query.js
+++ src/graphql/resolvers/Query.js
@@ -1,20 +1,19 @@
 import { AuthenticationError, ForbiddenError } from 'apollo-server-express';
 import User from '../../models/User';
-import * as error from './messages';
+import * as error from '../messages';
+import { getUserId } from './utils';
 
 const Query = {
   hello: () => 'world',
   // stories: (parent, args, context) => context.prisma.stories(),
   // story: (parent, { id }, context) => context.prisma.story({ id }),
-  // me: (parent, args, context) => {
-  //   const id = getUserId(context);
-  //   return context.prisma.user({ id });
-  // },
   users: (parent, args, context) => {
-    // console.log(context.loggedInUser);
-
     if (!context.loggedInUser) throw new ForbiddenError(error.auth.failed);
     return User.find({});
+  },
+  user: (parent, args, context) => {
+    if (!context.loggedInUser) throw new ForbiddenError(error.auth.failed);
+    return User.findById(args.id);
   },
 };
 
